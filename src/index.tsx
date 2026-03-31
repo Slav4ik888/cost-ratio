e161c57fac7af7ec5c7381e08dc2934d95723f25
Vyacheslav <korzan.va@mail.ru>
2026-03-31 20:36:29 +0300

    fixed webpack config 2

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ root.render(
 );
 
 
-// git add . && git commit -m "fixed webpack config" && git push -u origin master
+// git add . && git commit -m "fixed webpack config 2" && git push -u origin master
 // git add . && git commit -m "ended refact" && git push -u origin migrate-to-ts
 
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,11 @@
 import path from 'path';
 import { Configuration } from 'webpack';
 import { buildWebpackConfig } from './config/build/build-webpack-config';
-// import { BuildEnv, BuildPaths, BuildProject } from './config/build/types';
+import { BuildEnv, BuildPaths, BuildProject } from './config/build/types';
 
 
-export default (env) => {
-  const paths = {
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
     entry   : path.resolve(__dirname, 'src', 'index.tsx'),
     build   : path.resolve(__dirname, 'build'),
     html    : path.resolve(__dirname, 'public', 'index.html'),
@@ -17,7 +17,7 @@ export default (env) => {
   const isDev = mode === 'development';
   const project = BuildProject.FRONTEND;
 
-  const config = buildWebpackConfig({
+  const config: Configuration = buildWebpackConfig({
     mode,
     paths,
     isDev,
