d119012eb5edb7af49740f2cc3908579fb558cb7
Vyacheslav <korzan.va@mail.ru>
2026-03-31 19:13:59 +0300

    fixed package.json

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ root.render(
 );
 
 
-// git add . && git commit -m "fixed some 2" && git push -u origin master
+// git add . && git commit -m "fixed package.json" && git push -u origin master
 // git add . && git commit -m "ended refact" && git push -u origin migrate-to-ts
 
 
